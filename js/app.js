5fda788939fdcad167467f10b261379f7eae3895
Hibanelson <76537005+Hibanelson@users.noreply.github.com>
2021-08-24 07:02:54 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,12 @@ for(let i = 0;i < sections.length;i++) {
   let navbarMenuLink = document.createElement("a"); // create anchor element
   navbarMenuLink.innerHTML = sections[i].getAttribute("data-nav"); // add section name to the anchor element
   navbarMenuLink.classList.add("menu__link"); // add css styling to the anchor element
-  navbarMenuLink.setAttribute("href",`#section${i + 1}`); // add href to the anchor element
+  navbarMenuLink.setAttribute("id",`Section ${i + 1}`); // add id to the anchor element
+  //  link each navbar item to the corresponding section
+  navbarMenuLink.onclick = () => {
+      let id = navbarMenuLink.getAttribute("id");
+      document.querySelector(`section[data-nav="${id}"]`).scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+  }
   navbarMenuLi.appendChild(navbarMenuLink); // add the anchor element to the list item element
   navbarList.appendChild(navbarMenuLi); // add the list item element to the nav
 }
@@ -35,4 +40,28 @@ sections.forEach((section) => {
     },options);
     observer.observe(section);
 })
+// define Intersection Observer API options to highlight nav
+let options2 = {
+    root : null ,
+    rootMargin : "0px",
+    threshold : 0.7      
+};
+// define Intersection Observer object to highlight nav
+sections.forEach((section) => {
+    let observer = new IntersectionObserver((entries,observer) => {
+     entries.forEach(entry => {
+         if(entry.target.className.search("your-active-class") !== -1){
+             document.getElementById(entry.target.getAttribute("data-nav")).classList.remove("active-link");
+         }
+     });
+     entries.forEach(entry => {
+       if(entry.isIntersecting) {
+           document.getElementById(entry.target.getAttribute("data-nav")).classList.add("active-link");
+       }
+     });
+    },options2);
+    observer.observe(section);
+})
+
+
 
